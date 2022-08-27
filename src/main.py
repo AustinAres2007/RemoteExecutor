9320575a4dd138003b8ae01951c71d82bc83050c
AustinAres2007 <hakuares@gmail.com>
2022-08-28 00:14:20 +0100

    experimentation with classes and gitignore updates

--- src/main.py
+++ src/main.py
@@ -1,35 +1,48 @@
-import socket, threading
+import socket, sys
 from typing import *
 
 # I do not bother to use asyncio. This is meant for one connection at a time.
 
-HOST = "192.168.1.227"
-PORT = 2022
-RUN = True
+def main():
+    HOST = PORT = None
+    try:
+        HOST = sys.argv[1]
+        PORT = int(sys.argv[2])
+    except IndexError:
+        return print("Missing Arguments")
+    else:
+        print(f"~ Details ~\n\nHost IP: {HOST}\nHost Port: {PORT}\n\n")
+        with RemoteExecutor(socket.AF_INET, socket.SOCK_STREAM) as code_host:
+            code_host.bind((HOST, PORT))
+            code_host.listen(1)
+
+            print('Host listening for incoming connections.')
+
+            code_host.start()
 
 # TODO: Handle client error handling (Here)
+
+class Client(socket.socket):
+    def __init__(self, client: socket.socket):
+        self.client = client
 
 class RemoteExecutor(socket.socket):
     def __init__(self, *args, **kwargs):
         self.command_list = {
-            "project": self.file_transfer
+            "project": self.file_transfer,
+            "exit": self._close_client
         }
         super().__init__(*args, **kwargs)
 
     def __str__(self):
         return f"RemoteExecutor(host={self.host}, port={self.port})"
 
-    def file_transfer(self, command, client) -> list[Any, str]:
-        client.sendall(b'wf')
-
-        with open("TEST.txt", 'wb+') as file_to_write:
-            while True:
-                chunk = client.recv(4096)
-                print(chunk)
-                if chunk:
-                    file_to_write.write(chunk)
-                else:
-                    break
+    def file_transfer(self, command: str, client: Client) -> list[Any, str]:
+        pass
+    
+    def _close_client(self, cmd: str, client: Client):
+        client.close()
+        client = None
 
     def process_client(self, client: socket.socket) -> list[str, str]:
         command = client.recv(1024).decode('utf-8')
@@ -41,25 +54,20 @@ class RemoteExecutor(socket.socket):
         
         return [command, reply]
     
-    def relationship(self) -> None:
-        while RUN:
+    def start(self) -> None:
+        while True:
             client, addr = super().accept()
-            command = None
+            client = Client(client).client
+
+            print(client.__str__())
 
             print(f"Connection from {addr}")
 
-            while command != 'terminate':
+            while client:
                 command, reply = self.process_client(client=client)
                 client.sendall(reply.encode())
-            else:
-                print(f"Connection terminated by {addr}")
-                client.close()
-        
-    
-with RemoteExecutor(socket.AF_INET, socket.SOCK_STREAM) as code_host:
-    code_host.bind((HOST, PORT))
-    code_host.listen(1)
-    code_host.relationship()
-
+            
+if __name__ == "__main__":
+    main()
 
         
