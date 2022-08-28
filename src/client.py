bf9af63724c2fcb87851c88e3fb9083ca1aa36b0
Ausitin Ares <62123783+AustinAres2007@users.noreply.github.com>
2022-08-28 01:43:52 +0100

    Client script

---
+++ src/client.py
@@ -0,0 +1,34 @@
+import socket, os, sys
+
+SERVER_HOST: str = "192.168.1.115"
+SERVER_PORT: int = 2022
+
+# TODO: handle file sending
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as re_client:
+
+        def close():
+            re_client.close()
+            sys.exit(0)
+
+        re_client.connect((SERVER_HOST, SERVER_PORT))
+        commands = {
+                "exit": lambda: close()
+        }
+        client_side_only = ['exit']
+
+        while True:
+            command = input(f"Send to {SERVER_HOST}:{SERVER_PORT} >>> ")
+
+            try:
+                commands[command]()
+                if command not in client_side_only:
+                    re_client.sendall(command.encode())
+                    print(re_client.recv(1024).decode('utf-8'))
+
+            except KeyError:
+                print(f"{command} is not a client command.")
+
+if __name__ == "__main__":
+    main()
