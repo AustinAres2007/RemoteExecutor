123516a2f35638b424397e48683bb6f431f9211c
AustinAres2007 <hakuares@gmail.com>
2022-08-30 00:13:34 +0100

    Tests for File type

--- src/classes/message.py
+++ src/classes/message.py
@@ -1,13 +1,24 @@
-import sys, io
+import sys, io, os
 
 class File(object):
     """Represents a File to be send over a network."""
-    def __new__(cls, *args, **kwargs) -> io.TextIOWrapper:
-        return super().__init__(*args, **kwargs)
-    
-    def __init__(self, *args, **kwargs):
-        pass
 
+    def __repr__(self) -> str:
+        return f'<file={self.__file__}, mode="{self.__mode__}"'
+
+    def __init__(self, file, mode, *args, **kwargs):
+        self.__file__ = open(file, mode, *args, **kwargs)
+        self.__filesize__ = self.__file__.read().__sizeof__()
+        super().__init__()
+
+    def file(self) -> io.BufferedReader:
+        return self.__file__
+    
+    def mode(self) -> str:
+        return self.__file__.mode
+
+    def data(self):
+        return self.__file__.read()
 
 class Message(str):
     """Represents a Message to be send over a network."""
@@ -16,10 +27,10 @@ class Message(str):
         return self
 
     def __str__(self) -> str:
-        return f"Message(str={super().__str__()}, file={self.file})"
+        return f'Message(str={super().__str__()}, file="{self.file}")'
 
     def __new__(cls, *args, **kwargs):
-        cls.file: io.FileIO = kwargs['file']
+        cls.file: File = kwargs['file']
         del kwargs['file']
 
         print(cls.file)
@@ -33,5 +44,5 @@ class Message(str):
         return self.encode()
 
 if __name__ == '__main__':
-    file_obj = File('demo_file.py', 'rb')
-    print(file_obj.what_is_self())
+    file_obj = File(file='demo_file.py', mode='rb')
+    print(file_obj.file())
