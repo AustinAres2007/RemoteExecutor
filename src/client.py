314f51627c61893bcfeb49dd783e224b37df187d
Ausitin Ares <62123783+AustinAres2007@users.noreply.github.com>
2022-08-30 14:32:11 +0100

    Client error handlers and added to .gitignore
    None

--- src/client.py
+++ src/client.py
@@ -1,60 +1,55 @@
 import socket, os, sys, io
-
-SERVER_HOST: str = "demp.ddns.net"
-SERVER_PORT: int = 2022
+from classes.message import Message, File
 
 # TODO: handle file sending
-
-class Message(str):
-
-    def __repr__(self) -> str:
-        return self.string
-
-    def __str__(self) -> str:
-        return f"Message(str={self.string})"
-
-    def __init__(self, string: str, file: io.FileIO):
-        self.string = string
-        self.file = file
+def error(string):
+    return print(string); sys.exit(1)
     
-    def size(self) -> int:
-        return sys.getsizeof(self.string)
-    
-    def in_bytes(self) -> bytes:
-        return self.string.encode()
-
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as re_client:
-
+    try:
+        SERVER_HOST: str = sys.argv[1]
+        SERVER_PORT: int = int(sys.argv[2])
+    except IndexError:
+        return error("Not enough parameters in run arguments.")
+    except ValueError:
+        return error("The port parameter is a string type, not integer.")
+    else:
         def close():
             re_client.close()
-            sys.exit(0)
-
-        re_client.connect((SERVER_HOST, SERVER_PORT))
-        commands = {
-                "exit": lambda: close(),
-                "status": lambda: None,
-                "shutdown": lambda: None,
-                "project": lambda: None
-        }
-        client_side_only = ['exit']
-
-        while True:
-            try:
-
-                command = input(f"Send to {SERVER_HOST}:{SERVER_PORT} >>> ")
-
+            return error("Shutting down client.")
             
-                commands[command]()
-                if command not in client_side_only:
-                    re_client.sendall(command.encode())
-                    reply = re_client.recv(1024).decode('utf-8')
-                    print(reply)
-
-            except KeyError:
-                print(f"{command} is not a client command.")
-            except KeyboardInterrupt:
-                sys.exit(0)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as re_client:
+            try:
+    
+                re_client.connect((SERVER_HOST, SERVER_PORT))
+                commands = {
+                        "exit": lambda: close(),
+                        "status": lambda: None,
+                        "shutdown": lambda: None,
+                        "project": lambda: None
+                }
+                client_side_only = ['exit']
+                
+            except (ConnectionRefusedError, ConnectionError):
+                return error("Server refused connection / the server does not exist.")
+            
+            else:
+                while True:
+                    try:
+        
+                        command = input(f"Send to {SERVER_HOST}:{SERVER_PORT} >>> ")
+        
+                    
+                        commands[command]()
+                        if command not in client_side_only:
+                            re_client.sendall(command.encode())
+                            reply = re_client.recv(1024).decode('utf-8')
+                            print(reply)
+        
+                    except KeyError:
+                        print(f"{command} is not a client command.")
+                    except KeyboardInterrupt:
+                        return error("Shutting down client.")
 
 if __name__ == "__main__":
     main()
