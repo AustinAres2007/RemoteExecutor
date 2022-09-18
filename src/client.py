282716aecfdfee1b5938e227ee6ca2ecc43171bd
AustinAres2007 <hakuares@gmail.com>
2022-09-19 00:51:31 +0100

    Major changes, a "base" has been made

--- src/client.py
+++ src/client.py
@@ -1,53 +1,69 @@
+from curses.ascii import isdigit
 import socket, os, sys, io, pickle
 from classes.message import Message, File
-
+from threading import Thread
 # TODO: handle file sending
 def error(string):
     return print(string); sys.exit(1)
-    
+
 def main():
+    # Opens a line of communication between client and host.
+    def listen_for_messages():
+        recv_type = 1
+        buffer_size = 4096
+
+        while True:
+            print("", end=" ")
+            reply_data = re_client.recv(buffer_size)
+            if recv_type:
+                buffer_size = int(reply_data.decode())
+                recv_type = 0
+            else:
+                reply = pickle.loads(reply_data)
+                print(f"\n{reply.message}")
+                recv_type = 1
+
     try:
         SERVER_HOST: str = sys.argv[1]
         SERVER_PORT: int = int(sys.argv[2])
     except IndexError:
         return error("Not enough parameters in run arguments.")
     except ValueError:
-        return error("The port parameter is a string type, not integer.")
+        return error("The port parameter is a string type, not integer. (File Argument format: <host-ip> <port>)")
     else:
-        def close():
-            re_client.close()
-            return error("Shutting down client.")
             
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as re_client:
             try:
     
                 re_client.connect((SERVER_HOST, SERVER_PORT))
                 commands = {
-                        "exit": lambda: close(),
-                        "status": lambda: None,
-                        "shutdown": lambda: None,
-                        "project": lambda: None
+                        "status": (lambda: None, False),
+                        "download": (lambda: None, False),
+                        "echo": (lambda: None, False),
+                        "exit": (lambda: None, False),
+                        "sys": (lambda: None, False)
                 }
-                client_side_only = ['exit']
                 
             except (ConnectionRefusedError, ConnectionError):
                 return error("Server refused connection / the server does not exist.")
             
             else:
+                message_thread = Thread(target=listen_for_messages)
+                message_thread.start()
+
                 while True:
                     try:
-        
-                        command = input(f"Send to {SERVER_HOST}:{SERVER_PORT} >>> ")
-        
-                    
-                        commands[command]()
-                        if command not in client_side_only:
-                            re_client.sendall(command.encode())
-                            reply = pickle.loads(re_client.recv(4096))
-                            print(reply)
+                        command_name = input(f"Send to {SERVER_HOST}:{SERVER_PORT} >>> ")
+                        command = commands[command_name.split(' ')[0]]
+                        command[0]()
+
+                        if not command[1]:
+                            re_client.sendall(command_name.encode())
         
                     except KeyboardInterrupt:
                         return error("Shutting down client.")
+                    except KeyError:
+                        print("Specified command does not exist.")
 
 if __name__ == "__main__":
     main()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-import socket, sys, pickle
+import socket, sys, pickle, os
 from typing import *
 from classes.message import *
+from git import Repo
+
 
 # Don't use this code if you do not like having risks. This script uses Pickle to send class objects.
 # Use at your own risk. (Similar risks to using eval() or exec())
 
 # I do not bother to use asyncio. This is meant for one connection at a time.
-
 
 def main():
 
@@ -33,47 +34,77 @@ class RemoteExecutor(socket.socket):
 
     def __init__(self, *args, **kwargs):
         self.command_list = {
-            "project": self.file_transfer,
-            "shutdown": lambda *args: self.close(),
-            "status": lambda *args: self.status()
-
+            "status": lambda *a: self.status(*a), 
+            "clone": lambda *a: self.download_repo(*a),
+            "echo": lambda *a: self.echo_to_client(*a),
+            "exit": lambda *a: self.close_connection(*a),
+            "sys": lambda *a: self.terminal_command(*a)
         }
         super().__init__(*args, **kwargs)
 
-    def status(self) -> str:
-        return "200"
+    def send_message(self, message: str):
+        msg = Message(str(message), None, self.host)
+        self.client.sendall(str(len(message)+sys.getsizeof(msg)+4096).encode())
+        self.client.sendall(pickle.dumps(msg))  
+    
+    # Server Commands
+    
+    def terminal_command(self, *args):
+        command = ' '.join(args)
+        cmd_resp = os.popen(command).read()
+        self.send_message(cmd_resp)
 
-    def file_transfer(self, command: str, client: socket.socket) -> str:
-        return "This command is not finished."
+    def close_connection(self, *args):
+        self.close_connection()
+        
+        sys.exit(0)
+
+    def echo_to_client(self, *args):
+        self.send_message(' '.join(args))
+
+    def download_repo(self, *args):
+        try:
+            repo = args[0]
+            saved_as = args[1]
+        except IndexError:
+            return self.send_message("Missing arguments.")
+
+        self.send_message(f'Downloading "{repo}"..')
+        os.mkdir(f"src/scripts/{saved_as}")
+        Repo.clone_from(repo, f'src/scripts/{saved_as}')
+
+        self.send_message(f'Finished downloading "{repo}".\nTo run, use "run src/scripts/{saved_as}"')
+
+    def status(self, *args) -> str:
+        self.send_message("200")
 
     def bind(self, __address) -> None:
         self.host = __address
         return super().bind(__address)
 
     def process_client(self, client: socket.socket) -> str:
-        command = client.recv(1024).decode('utf-8')
+        client_message = client.recv(1024).decode('utf-8').split(' ')
+        command = client_message[0]
+        args = client_message[1:]
+        self.command_list[command](*args) if command in self.command_list else "This command does not exist."
         
-        try:
-            reply = self.command_list[command](command, client)
-        except KeyError:
-            reply = "Command does not exist."
-        
-        return reply
-    
     def start(self) -> None:
         while True:
             try:
-                client, addr = super().accept()
+                self.client, addr = super().accept()
                 print(f"Connection from {addr[0]}")
-
-                while client:
-                    reply = Message(self.process_client(client=client), None, self.host)
-                    client.sendall(pickle.dumps(reply))            
+                self.send_message("""You are using this tool with the knowledge that this tool
+                    could be unsafe for public use. DO NOT let a person you do not trust use this tool, 
+                    as they can access the host computer with terminal or shell commands."""
+                )
+                while self.client:
+                    self.process_client(client=self.client)
+                              
             except OSError:
                 continue
             except (ConnectionAbortedError, ConnectionResetError, BrokenPipeError):
                 print(f"{addr[0]} Disconnected.")
-                client = None
+                self.client = None
             except AttributeError:
                 sys.exit(0)
 if __name__ == "__main__":
