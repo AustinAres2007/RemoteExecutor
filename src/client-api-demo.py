a9367054d5eb009608ec69efe9c31f8fb5f8d2ea
AustinAres2007 <hakuares@gmail.com>
2022-10-10 19:52:08 +0100

    Removed my local IP because I am dumb, also added password

--- src/client-api-demo.py
+++ src/client-api-demo.py
@@ -3,7 +3,11 @@ from client import *
 def get_message(msg):
     print(msg)
 
-with RemoteExecutorClient("192.168.1.190", 2023) as client_instance:
+IP = "0.0.0.0"
+PORT = 2022
+PASSWORD = ""
+
+with RemoteExecutorClient(IP, PORT) as client_instance:
     try:
         while True:
             client_instance.send_command(input("Remote Executor > "), get_message, ignore_unknown=True)  
